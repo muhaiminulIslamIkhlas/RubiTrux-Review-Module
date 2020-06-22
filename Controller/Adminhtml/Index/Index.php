ebfdae0df03bc11a00ed0e049ddb7cdef3f8a9bc
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2020-06-22 19:29:54 +0600

    Code rearrange for fetching and data save

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -31,6 +31,15 @@ class Index extends \Magento\Backend\App\Action {
     }
     
     public function saveTODatabase($jsonData){
+        
+        print_r($jsonData);
+        $additional_param = array();
+        $additional_param['social'] = $jsonData[0]['reviews'][0]['social'];
+        $additional_param['products_purchased'] = $jsonData[0]['reviews'][0]['products_purchased'];
+//            print_r($additional_param);
+//            print_r($jsonData);
+        $additionalParam = json_encode($additional_param);
+        
         $model = $this->_dataExample->create();
         $model->addData([
             "product_id" => 'Title 01',
@@ -68,76 +77,7 @@ class Index extends \Magento\Backend\App\Action {
         
     }
 
-    public function execute() {
-        
-        
-        
-        
-        
-        /* Fetch data */
-        $url = "https://api.feefo.com/api/10/importedreviews/product?page=1&page_size="
-                . "10&since_period=ALL&full_thread=include&unanswered_feedback=include&source="
-                . "on_page_product_integration&sort"
-                . "=-created_date&feefo_parameters=include&media=include&merchant_identifier"
-                . "=rubitrux&origin=www.rubitrux.com&parent_product_sku=6563";
-        $parts = parse_url($url);
-
-        $curlSession = curl_init();
-        curl_setopt($curlSession, CURLOPT_URL, $url);
-        curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-
-        $jsonData = json_decode(curl_exec($curlSession), true);
-        curl_close($curlSession);
-
-        $jsonData = array($jsonData);
-        
-         /* XXXXX Fetch data XXXXX */
-        
-        
-        
-        
-        //Additional Param
-        
-        $additional_param = array();
-        $additional_param['social'] = $jsonData[0]['reviews'][0]['products'][0]['social'];
-        $additional_param['products_purchased'] = $jsonData[0]['reviews'][0]['products_purchased'];
-//            print_r($additional_param);
-//            print_r($jsonData);
-        $additionalParam = json_encode($additional_param);
-        
-        
-        
-  /* Data save in database */
-        $model = $this->_dataExample->create();
-        $model->addData([
-            "product_id" => 'Title 01',
-            "rating_value" => $jsonData[0]['reviews'][0]['products'][0]['rating']['rating'],
-            "review_id" => $jsonData[0]['reviews'][0]['products'][0]['id'],
-            "review" => $jsonData[0]['reviews'][0]['products'][0]['review'],
-            "reviews_url" => $jsonData[0]['reviews'][0]['url'],
-            "customer_display_name" => 1,
-            "created_at" => $jsonData[0]['reviews'][0]['products'][0]['created_at'],
-            "last_updated_date" => $jsonData[0]['reviews'][0]['last_updated_date'],
-            "additional_param" => $additionalParam
-        ]);
-        $saveData = $model->save();
-   /* XXXX Data save in database XXXXX */
-        
-        
-        
-        
-        /*This is for overall data*/
-//        $collection = $this->productCollectionFactory->create();
-//        $collection->addFieldToSelect('id');
-//        var_dump( $collection->getData());
-        /*-----This is for overall data-----*/
-        
-        
-        
-        
-        
-        /*This is for specific column*/
+    public function execute() {       
         $collection = $this->productCollectionFactory->create();
         $collection->getSelect()
                 ->reset(\Zend_Db_Select::COLUMNS)
@@ -145,31 +85,22 @@ class Index extends \Magento\Backend\App\Action {
        // print_r( $collection->getData());
         
         foreach($collection->getData() as $item){
-            //array_push($productId,$item['entity_id']);
-                    $url = "https://api.feefo.com/api/10/importedreviews/product?page=1&page_size="
-                . "10&since_period=ALL&full_thread=include&unanswered_feedback=include&source="
-                . "on_page_product_integration&sort"
-                . "=-created_date&feefo_parameters=include&media=include&merchant_identifier"
-                . "=rubitrux&origin=www.rubitrux.com&parent_product_sku=".$item['entity_id'];
-                
-                        $parts = parse_url($url);
+                $jsonData = $this->fetchData($item['entity_id']);
+                if(!empty($jsonData[0]['reviews'])){
+                    $this->saveTODatabase($jsonData);
+                }
+                //print_r($jsonData);
+            }
+            echo "Done";
 
-        $curlSession = curl_init();
-        curl_setopt($curlSession, CURLOPT_URL, $url);
-        curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-
-        $jsonData = json_decode(curl_exec($curlSession), true);
-        curl_close($curlSession);
-
-        $jsonData = array($jsonData);
-        
-        print_r($jsonData);
-        }
-        
-        //print_r($productId);
-        /*-----This is for specific column-----*/
         exit();
     }
 
 }
+
+
+/* For testing 
+ *         $collection = $this->productCollectionFactory->create();
+           $collection->addFieldToSelect('id');
+           var_dump( $collection->getData());
+ */
